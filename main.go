a5122220ec11dc21252077cc9f232a297bbd1edf
v_wensqi <v_wensqi@tencent.com>
2025-04-16 14:17:11 +0800

    e=>{e.report({telemetry:{id:r.telemetry.id||"",model:r.telemetry.model||""}})}feat: 重构Web服务器并新增多页面模板与功能

--- main.go
+++ main.go
@@ -1,42 +1,152 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
+	"text/template"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
-type Person struct {
-	Name string
-}
-
-type Post struct {
-	Person Person
-	Body   string
-}
-
-type TUser struct {
-	Title  string
-	Person Person
-	Posts  []Post
+func UnixToTime(timeStamp int) string {
+	t := time.Unix(int64(timeStamp), 0)
+	return t.Format("2006-01-02 15:04:05")
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		p1 := Person{Name: "Taro"}
-		p2 := Person{Name: "Jiro"}
-
-		posts := []Post{
-			Post{Person: p1, Body: "Hello"},
-			Post{Person: p2, Body: "Hello2"},
-		}
-
-		v := TUser{
-			Title:  "Hello, This is Homepage !",
-			Person: p1,
-			Posts:  posts,
-		}
-		tpl, _ := template.ParseFiles("./templates/index.html")
-		tpl.Execute(w, &v)
+	r := gin.Default()
+	r.SetFuncMap(template.FuncMap{
+		"UnixToTime": UnixToTime,
 	})
-	http.ListenAndServe(":8080", nil)
+
+	r.LoadHTMLGlob("templates/**/*")
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World!")
+	})
+	r.GET("/test", func(c *gin.Context) {
+		c.String(200, "There is test page!")
+	})
+	r.GET("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(200, "Hello %s", name)
+	})
+	r.POST("/post", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Post老子干什么？",
+		})
+	})
+	r.PUT("/edit", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Put老子干什么？",
+		})
+	})
+	r.DELETE("/delete", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Delete老子干什么？",
+		})
+	})
+	type User struct {
+		Name  string `json:"name"`
+		Age   int    `json:"age"`
+		Email string `json:"email"`
+	}
+
+	// r.GET("/user", func(c *gin.Context) {
+	// 	user := User{
+	// 		Name:  "小明",
+	// 		Age:   18,
+	// 		Email: "xiaoming@163.com",
+	// 	}
+	// 	c.JSON(200, user)
+	// })
+
+	r.GET("/tom", func(c *gin.Context) {
+		tom := User{
+			Name:  "Tom",
+			Age:   18,
+			Email: "tom@163.com",
+		}
+		c.JSONP(200, tom)
+	})
+
+	r.GET("/xml", func(c *gin.Context) {
+		c.XML(http.StatusOK, gin.H{
+			"name":  "小明",
+			"age":   18,
+			"email": "xiaoming@163.com",
+		})
+	})
+
+	r.GET("/news", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "news.html", gin.H{
+			"title": "新闻",
+		})
+	})
+
+	r.GET("/long", func(c *gin.Context) {
+
+		jack := User{
+			Name:  "jack",
+			Age:   18,
+			Email: "jack@163.com",
+		}
+
+		c.HTML(http.StatusOK, "long.html", gin.H{
+			"user":  jack,
+			"title": "新闻",
+		})
+	})
+
+	r.GET("/admin", func(c *gin.Context) {
+		admin := User{
+			Name:  "Admin",
+			Age:   18,
+			Email: "admin@163.com",
+		}
+		tom := User{
+			Name:  "Tom",
+			Age:   18,
+			Email: "tom@163.com",
+		}
+		jack := User{
+			Name:  "jack",
+			Age:   18,
+			Email: "jack@163.com",
+		}
+		c.HTML(http.StatusOK, "admin/index.html", gin.H{
+			"title": "Admin",
+			"admin": admin,
+			"Tags":  []string{"tag1", "tag2", "tag3"},
+			"Items": []interface{}{
+				// &User{
+				// 	Name:  "Tom",
+				// 	Age:   18,
+				// 	Email: "tom@163.com",
+				// },
+				// &User{
+				// 	Name:  "jack",
+				// 	Age:   18,
+				// 	Email: "jack@163.com",
+				// },
+				admin,
+				tom,
+				jack,
+			},
+		})
+	})
+
+	r.GET("/user", func(c *gin.Context) {
+
+		elon := User{
+			Name:  "Elon",
+			Age:   18,
+			Email: "elon@163.com",
+		}
+		c.HTML(http.StatusOK, "user/index.html", gin.H{
+			"title": "User",
+			"user":  elon,
+			"date":  1744707155,
+		})
+	})
+	r.Run(":8080")
 }
